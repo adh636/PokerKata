4d008d03e2403c1a0a7d253c5627f4792bf82e45
Adam <adh636@gmail.com>
2016-08-04 11:16:04 -0400

    add typescript/tdd seed to project

---
+++ system.conf.js
@@ -0,0 +1,10 @@
+System.config({
+  defaultJSExtensions: true,
+
+  map: {
+    'typescript': 'node_modules/typescript/lib/typescript.js',
+    'systemjs': 'node_modules/systemjs/dist/system.js',
+    'system-polyfills': 'node_modules/systemjs/dist/system-polyfills.js',
+    'lodash': 'node_modules/lodash/lodash.js'
+  }
+});
