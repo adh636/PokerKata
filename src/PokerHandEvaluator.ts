7b22e43674231f36be2190e517005230559ac1b1
Adam Hilliard <adh636@gmail.com>
2016-08-28 22:30:59 -0400

    add card description map

--- src/PokerHandEvaluator.ts
+++ src/PokerHandEvaluator.ts
@@ -21,6 +21,21 @@ export class Card {
         "K": 13,
         "A": 14
     };
+    cardDescriptionMap: any = {
+        2: "two",
+        3: "three",
+        4: "four",
+        5: "five",
+        6: "six",
+        7: "seven",
+        8: "eight",
+        9: "nine",
+        10: "ten",
+        11: "jack",
+        12: "queen",
+        13: "king",
+        14: "ace"
+    }
     
     constructor(card: string) {
         this.setCard(card);
