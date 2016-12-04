f8442b6b530fa01123ed0e8f3ba40c0864659a3b
Adam <adh636@gmail.com>
2016-12-03 21:56:07 -0500

    refactor pokerkata. test high card, full house, flush

--- spec/PokerHandEvaluaterTests.ts
+++ spec/PokerHandEvaluaterTests.ts
@@ -1,79 +1,39 @@
-import {Card, Hand, Dealer} from "../src/PokerHandEvaluator";
-
+import {Card, Hand} from "../src/PokerHandEvaluator";
 describe("PokerHandEvaluator", () => {
-    
-    describe("high card", () => {
-        it("should have 3 beat 2", () => {
-            let three: Card = new Card("3C");
-            let two: Card = new Card("2C");
-            expect(three.value > two.value).toBeTruthy();
+    describe("Card", () => {
+        it("should value three as 3", () => {
+            let three: Card = new Card("3H");
+            expect(three.value).toEqual(3);
         });
-        it("should have T beat 9", () => {
-            let ten: Card = new Card("TC");
-            let nine: Card = new Card("9C");
-            expect(ten.value > nine.value).toBeTruthy();
+        it("should value king as 13", () => {
+            let king: Card = new Card("KH");
+            expect(king.value).toEqual(13);
         });
     });
-    
-    describe("hand value", () => {
-        it("should get high card", () => {
-            let hand: Hand = new Hand("Black: 4C 3C 2C 5C 7H");
-            let highCard: number = hand.cardValues[0];
-            expect(highCard).toEqual(7);
+    describe("Hand", () => {
+        it("should get high card in descending order", () => {
+            let nineHigh: Hand = new Hand("9H 8H 6H 4S 2S");
+            expect(nineHigh.cards[0].value).toEqual(9);
         });
-        it("should value high card only as 1", () => {
-            let hand: Hand = new Hand("Black: 4C 3C 2C 5C 7H");
-            let handValue: number = hand.handValue;
-            expect(handValue).toEqual(1);
+        it("should get high card when unordered", () => {
+            let nineHigh: Hand = new Hand("2H 8H 6H 4S 9S");
+            expect(nineHigh.cards[0].value).toEqual(9);
         });
-        it("should value pair as 2", () => {
-            let pair: Hand = new Hand("Black: 4C 3C 2C 5C 4H");
-            let handValue: number = pair.handValue;
-            expect(handValue).toEqual(2);
+        it("should describe high card", () => {
+            let nineHigh: Hand = new Hand("9H 8H 6H 4S 2S");
+            expect(nineHigh.description).toEqual("high card: 9");
         });
-        it("should value two pair as 3", () => {
-            let twoPair: Hand = new Hand("Black: 4C 2S 2C 5C 4H");
-            let handValue: number = twoPair.handValue;
-            expect(handValue).toEqual(3);
+        it("should describe full house with three of a kind high full of pair low", () => {
+            let fullHouse: Hand = new Hand("2H 5C 5S 2S 5D");
+            expect(fullHouse.description).toEqual("full house");
         });
-        it("should value three of a kind as 4", () => {
-            let threeOfAKind: Hand = new Hand("Black: 2C 2H 2S 5C 4H");
-            let handValue: number = threeOfAKind.handValue;
-            expect(handValue).toEqual(4);
+        it("should describe full house with three of a kind low full of pair high", () => {
+            let fullHouse: Hand = new Hand("2H 2C 5S 2S 5D");
+            expect(fullHouse.description).toEqual("full house");
         });
-        it("should value straight as 5", () => {
-            let straight: Hand = new Hand("Black: 2C 3H 4S 5C 6H");
-            let handValue: number = straight.handValue;
-            expect(handValue).toEqual(5);
-        });
-        it("should value flush as 6", () => {
-            let flush: Hand = new Hand("Black: 2C 9C 4C 5C 6C");
-            let handValue: number = flush.handValue;
-            expect(handValue).toEqual(6);
-        });
-        it("should value full house as 7", () => {
-            let fullHouse: Hand = new Hand("Black: 2C 2H 4C 2S 4S");
-            let handValue: number = fullHouse.handValue;
-            expect(handValue).toEqual(7);
-        });
-        it("should value four of a kind as 8", () => {
-            let fourOfAKind: Hand = new Hand("Black: 2C 2H 2D 2S 4S");
-            let handValue: number = fourOfAKind.handValue;
-            expect(handValue).toEqual(8);
-        });
-        it("should value straight flush as 9", () => {
-            let straightFlush: Hand = new Hand("Black: 2C 3C 4C 5C 6C");
-            let handValue: number = straightFlush.handValue;
-            expect(handValue).toEqual(9);
-        });
-    });
-
-    describe("best hand", () => {
-        it("should have hand with high card win", () => {
-            let blackHand: Hand = new Hand("Black: 4C 3C 2C 5C 7H");
-            let whiteHand: Hand = new Hand("White: 4C 3C 2C 5C 8H");
-            let dealer: Dealer = new Dealer();
-            expect(dealer.getWinner(blackHand, whiteHand)).toEqual("White");
+        it("should describe flush", () => {
+            let flush: Hand = new Hand("2H 3H 5H 6H 8H");
+            expect(flush.description).toEqual("flush");
         });
     });
 });
--- src/PokerHandEvaluator.ts
+++ src/PokerHandEvaluator.ts
@@ -1,185 +1,89 @@
-export class Dealer {
-
-    getWinner(blackHand:Hand, whiteHand:Hand) {
-        if (blackHand.handValue > whiteHand.handValue) return blackHand.playerName;
-        if (whiteHand.handValue > blackHand.handValue) return whiteHand.playerName;
-        for (let i = 0; i < 5; i++) {
-            if (blackHand.cardValues[i] > whiteHand.cardValues[i]) return blackHand.playerName;
-            if (whiteHand.cardValues[i] > blackHand.cardValues[i]) return whiteHand.playerName;
-        }
-        return "Tie";
-    }
-}
-
 export class Card {
-    name: string;
     value: number;
-    cardValueMap: any = {
-        "T": 10,
-        "J": 11,
-        "Q": 12,
+    faceCardValue: any = {
+        "A": 14,
         "K": 13,
-        "A": 14
+        "Q": 12,
+        "J": 11,
+        "T": 10
     };
-    cardDescriptionMap: any = {
-        2: "two",
-        3: "three",
-        4: "four",
-        5: "five",
-        6: "six",
-        7: "seven",
-        8: "eight",
-        9: "nine",
-        10: "ten",
-        11: "jack",
-        12: "queen",
-        13: "king",
-        14: "ace"
-    }
-    
+    suit: string;
+
     constructor(card: string) {
-        this.setCard(card);
+        this.setCardProperties(card);
     }
 
-    private setCard(card: string) {
-        this.name = card;
-        if (parseInt(card[0])) {
-            this.value = parseInt(card[0]);
-        }
-        else {
-            this.value = this.cardValueMap[card[0]];
+    private setCardProperties(card: string) {
+        this.setCardSuit(card);
+        this.setCardValue(card);
+    }
+
+    private setCardSuit(card: string) {
+        this.suit = card[1];
+    }
+
+    private setCardValue(card: string) {
+        this.value = parseInt(card[0]);
+        if (!this.value) {
+            this.value = this.faceCardValue[card[0]];
         }
     }
 }
 
 export class Hand {
     cards: Card[] = [];
-    cardValues: number[] = [];
-    handValue: number;
-    flush: boolean = true;
-    playerName: string;
-    // handValueMap: any = [
-    //     [this.isStraightFlush(), 9],
-    //     [this.isFourOfAKind(), 8],
-    //     [this.isFullHouse(), 7],
-    //     [this.isFlush(), 6],
-    //     [this.isStraight(), 5],
-    //     [this.isThreeOfAKind(), 4],
-    //     [this.isTwoPair(), 3],
-    //     [this.isPair(), 2]
-    // ];
-
-    func: any = this.isStraightFlush();
+    description: string;
 
     constructor(hand: string) {
-        this.setValues(hand);
-    }
-    private setValues(hand: string) {
-        this.setCardValues(hand);
-        this.handValue = this.getHandValue();
+        this.setCardsAndSort(hand);
+        this.setDescription();
     }
 
-    private setCardValues(hand:string) {
+    private setCardsAndSort(hand: string): void {
         let handArr = hand.split(" ");
-        this.playerName = handArr[0].substring(0, handArr[0].length-1);
-        let suit: string = handArr[1][1];
-        for (let i = 1; i < handArr.length; i++) {
-            this.cardValues.push(new Card(handArr[i]).value);
-            this.flush = (suit === handArr[i][1]);
+        this.cards = handArr.map((card) => {
+            return new Card(card);
+        });
+        this.cards.sort((a, b) => b.value - a.value);
+    }
+
+    private setDescription() {
+        this.setHighCard();
+        this.checkForFullHouse();
+        this.checkForFlush();
+    }
+
+    private setHighCard() {
+        this.description = "high card: " + this.cards[0].value;
+    }
+
+    private checkForFullHouse() {
+        if (this.threeOfAKindHighFullOfPairLow()
+            || this.threeOfAKindLowFullOfPairHigh()) {
+            this.description = "full house";
         }
-        this.cardValues.sort((a, b) => b - a);
     }
 
-    private getHandValue(): number {
-
-        // for (let hand of this.handValueMap) {
-        //     if (hand[0]) {
-        //         return hand[1];
-        //     }
-        // }
-
-        if (this.isStraightFlush()) return 9;
-        if (this.isFourOfAKind()) return 8;
-        if (this.isFullHouse()) return 7;
-        if (this.flush) return 6;
-        if (this.isStraight()) return 5;
-        if (this.isThreeOfAKind()) return 4;
-        if (this.isTwoPair()) return 3;
-        if (this.isPair()) return 2;
-        return 1;
+    private threeOfAKindLowFullOfPairHigh() {
+        return this.cards[0].value === this.cards[1].value
+            && this.cards[2].value === this.cards[3].value
+            && this.cards[2].value === this.cards[4].value;
     }
 
-    private isPair(): boolean {
-        for (let i = 0; i < this.cardValues.length-1; i++) {
-            if (this.cardValues[i] === this.cardValues[i+1]) {
-                return true;
+    private threeOfAKindHighFullOfPairLow() {
+        return this.cards[0].value === this.cards[1].value
+            && this.cards[0].value === this.cards[2].value
+            && this.cards[3].value === this.cards[4].value;
+    }
+
+    private checkForFlush() {
+        let firstSuit = this.cards[0].suit;
+        let cardsWithFirstSuit = this.cards.filter((card) => {
+                return card.suit === firstSuit;
             }
+        );
+        if (cardsWithFirstSuit.length === 5){
+            this.description = "flush";
         }
-        return false;
-    }
-
-    private isTwoPair() {
-        let pairCount = 0;
-        for (let i = 0; i < this.cardValues.length-1; i++) {
-            if (this.cardValues[i] === this.cardValues[i+1]) {
-                pairCount++;
-            }
-        }
-        if (pairCount === 2) {
-            return true;
-        }
-        return false;
-    }
-
-
-    private isThreeOfAKind() {
-        for (let i = 0; i < this.cardValues.length-2; i++) {
-            if (this.cardValues[i] === this.cardValues[i+1] && this.cardValues[i] === this.cardValues[i+2]) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private isStraight() {
-        for (let i = 0; i < this.cardValues.length-1; i++) {
-            if (this.cardValues[i] !== this.cardValues[i+1] + 1) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private isFullHouse() {
-        for (let i = 0; i < this.cardValues.length-2; i++) {
-            if (this.cardValues[i] === this.cardValues[i+1] && this.cardValues[i] === this.cardValues[i+2]) {
-                if (i === 0 && this.cardValues[3] === this.cardValues[4]) {
-                    return true;
-                }
-                if (i === 2 && this.cardValues[0] === this.cardValues[1]) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    private isFourOfAKind() {
-        for (let i = 0; i < this.cardValues.length-3; i++) {
-            if (this.cardValues[i] === this.cardValues[i+1] && this.cardValues[i] === this.cardValues[i+2]
-                && this.cardValues[i] === this.cardValues[i+3]) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private isStraightFlush = function() {
-        if (this.flush && this.isStraight()) return true;
-        return false;
-    }
-
-    private isFlush() {
-        return this.flush;
     }
 }
